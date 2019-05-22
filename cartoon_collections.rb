4cf06f4e0c629a3b51610c7847ece52292bad77e
Ashley Muller <serenehopes@gmail.com>
2019-05-22 05:19:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each |item| do
+   return item if cheese_types.include?(item)
+  end
 end
