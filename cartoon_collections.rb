7afdda2f3075e4d7d1c4765679dded25cd2b3244
Ashley Muller <serenehopes@gmail.com>
2019-05-22 05:21:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each do |item|
-   return item if cheese_types.include?(item)
+  array.detect |item| do
+    cheese_types.include?(item)
   end
 end
